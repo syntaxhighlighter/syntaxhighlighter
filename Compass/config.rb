20014c4319ff28d4abedf985e3410e6284dbadc4
Alex Gorbatchev <devnull@localhost>
2010-02-22 10:05:02 +0530

    Merged work files from dev HG to BitBucket.

---
+++ Compass/config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../Styles"
+sass_dir = "."
+images_dir = "images"
+# output_style = :compressed
+# output_style = :compact
+output_style = :expanded
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
