7a13f6e02b570151a00be3e5f4f689b7f7c8e2a5
Alex Gorbatchev <devnull@localhost>
2010-06-28 22:09:07 -0400

    Replaced old SASS files with newer SCSS.

--- compass/config.rb
+++ compass/config.rb
@@ -2,7 +2,7 @@
 project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../Styles"
+css_dir = "../styles"
 sass_dir = "."
 images_dir = "images"
 # output_style = :compressed
