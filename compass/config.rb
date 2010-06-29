821808ec29673aa7263a1c3d5c3d2c32a49d8619
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2010-06-29 10:34:05 -0400

    Removed comments from output CSS. Updated toolbar ? with better visuals. Fixed collapsed toolbar style. Fixed caption style.

--- compass/config.rb
+++ compass/config.rb
@@ -1,12 +1,14 @@
-# Require any additional compass plugins here.
-project_type = :stand_alone
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "../styles"
-sass_dir = "."
-images_dir = "images"
+environment   = :production
+project_type  = :stand_alone
+http_path     = "/"
+css_dir       = "../styles"
+sass_dir      = "."
+images_dir    = "images"
+sass_options  = {
+  :line_numbers => false,
+  :debug_info   => false
+}
+
 # output_style = :compressed
 # output_style = :compact
 output_style = :expanded
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
