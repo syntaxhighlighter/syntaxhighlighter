604faff2737197752614aff0781a8f198a782c3e
Randy Stauner <randy@magnificent-tears.com>
2011-09-24 19:46:49 -0700

    Join lines with '\r\n' to fix copy-paste on IE8
    otherwise windows seems to paste all on one line.
    doesn't seem to affect any other browsers.

--- scripts/shCore.js
+++ scripts/shCore.js
@@ -687,7 +687,8 @@ function eachLine(str, callback)
 	for (var i = 0; i < lines.length; i++)
 		lines[i] = callback(lines[i], i);
 		
-	return lines.join('\n');
+	// include \r to enable copy-paste on windows (ie8) without getting everything on one line
+	return lines.join('\r\n');
 };
 
 /**
