b4275dd221c9eb5c53e8e4776c721139db4e29fc
Randy Stauner <randy@magnificent-tears.com>
2011-09-27 10:05:08 -0700

    Color any other lines starting with +/- as changes

--- scripts/shBrushDiff.js
+++ scripts/shBrushDiff.js
@@ -10,8 +10,8 @@
 			{ regex: /^\-\-\- .*$/gm,	css: 'color2' },	// old file
 			{ regex: /^\s.*$/gm,			css: 'color1' },
 			{ regex: /^@@.*@@.*$/gm,	css: 'variable' },	// location
-			{ regex: /^\+[^\+]{1}.*$/gm,	css: 'string' },
-			{ regex: /^\-[^\-]{1}.*$/gm,	css: 'comments' }
+			{ regex: /^\+.*$/gm,		css: 'string' },	// additions
+			{ regex: /^\-.*$/gm,		css: 'comments' }	// deletions
 			];
 	};
 
