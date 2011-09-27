b36ce2b7cce56614be4d977254c64104c61aed88
Randy Stauner <randy@magnificent-tears.com>
2011-09-27 09:52:15 -0700

    Allow extra context after location markers

--- scripts/shBrushDiff.js
+++ scripts/shBrushDiff.js
@@ -9,7 +9,7 @@
 			{ regex: /^\+\+\+.*$/gm,		css: 'color2' },
 			{ regex: /^\-\-\-.*$/gm,		css: 'color2' },
 			{ regex: /^\s.*$/gm,			css: 'color1' },
-			{ regex: /^@@.*@@$/gm,			css: 'variable' },
+			{ regex: /^@@.*@@.*$/gm,	css: 'variable' },	// location
 			{ regex: /^\+[^\+]{1}.*$/gm,	css: 'string' },
 			{ regex: /^\-[^\-]{1}.*$/gm,	css: 'comments' }
 			];
