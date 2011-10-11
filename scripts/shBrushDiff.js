1ae9646ac656799a06010beeebe909c63e5f6c76
Itamar Syn-Hershko <itamar@code972.com>
2011-10-11 14:05:36 +0200

    Make diff brush a tiny bit stricter for coloring headers
    Instead of coloring a line as a header when it begins with three
    minuses or three pluses, check that there is a space after the 3rd
    plus or minus. This is just a tiny bit stricter than before, but
    will prevent some false positives.
    
    Source https://github.com/dougwilson/js-syntax-highlighter/commit/af402399abf92ee195d2992e76336620a5ca512e

--- scripts/shBrushDiff.js
+++ scripts/shBrushDiff.js
@@ -6,8 +6,8 @@
 	function Brush()
 	{
 		this.regexList = [
-			{ regex: /^\+\+\+ .*$/gm,	css: 'color2' },	// new file
-			{ regex: /^\-\-\- .*$/gm,	css: 'color2' },	// old file
+			{ regex: /^\+\+\+\s.*$/gm,	css: 'color2' },	// new file
+			{ regex: /^\-\-\-\s.*$/gm,	css: 'color2' },	// old file
 			{ regex: /^\s.*$/gm,		css: 'color1' },	// unchanged
 			{ regex: /^@@.*@@.*$/gm,	css: 'variable' },	// location
 			{ regex: /^\+.*$/gm,		css: 'string' },	// additions
