834cbc79868b256f6389c48179fa7a3e81d6792c
Itamar Syn-Hershko <itamar@code972.com>
2011-10-11 14:27:46 +0200

    Missing keyword

--- scripts/shBrushCSharp.js
+++ scripts/shBrushCSharp.js
@@ -6,7 +6,7 @@
 	function Brush()
 	{
 		var keywords =	'abstract as base bool break byte case catch char checked class const ' +
-						'continue decimal default delegate do double else enum event explicit ' +
+						'continue decimal default delegate do double else enum event explicit volatile ' +
 						'extern false finally fixed float for foreach get goto if implicit in int ' +
 						'interface internal is lock long namespace new null object operator out ' +
 						'override params private protected public readonly ref return sbyte sealed set ' +
