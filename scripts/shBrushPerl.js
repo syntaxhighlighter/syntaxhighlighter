a5fb6a32ed81fdf55b87da3d64b720efa66e3967
Randy Stauner <randy@magnificent-tears.com>
2011-10-27 18:19:42 -0700

    Space css attribute consistently

--- scripts/shBrushPerl.js
+++ scripts/shBrushPerl.js
@@ -39,9 +39,9 @@
 			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,	css: 'string' },
 			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,	css: 'string' },
 			{ regex: new RegExp('(\\$|@|%)\\w+', 'g'),				css: 'variable' },
-			{ regex: new RegExp(this.getKeywords(funcs), 'gm'),	css: 'functions' },
+			{ regex: new RegExp(this.getKeywords(funcs), 'gm'),		css: 'functions' },
 			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),	css: 'keyword' }
-		    ];
+		];
 
 		this.forHtmlScript(SyntaxHighlighter.regexLib.phpScriptTags);
 	}
