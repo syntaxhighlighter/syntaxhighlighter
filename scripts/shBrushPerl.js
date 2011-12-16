3c1037c6e617e34f511783fd7349405b4d91e022
Randy Stauner <randy@magnificent-tears.com>
2011-12-15 22:10:33 -0700

    Match pod that goes to the end (no =cut)
    Otherwise a =pod block w/o a =cut wasn't being matched as pod

--- scripts/shBrushPerl.js
+++ scripts/shBrushPerl.js
@@ -59,7 +59,7 @@
 			// currently ignoring single quote package separator and utf8 names
 			{ regex: /(?:&amp;|[$@%*]|\$#)[a-zA-Z_](\w+|::)*/g,   		css: 'variable' },
 			{ regex: /\b__(?:END|DATA)__\b[\s\S]*$/g,				css: 'comments' },
-			{ regex: /^=\w[\s\S]*?\n=cut\s*$/gm,					css: 'comments' },		// pod
+			{ regex: /(^|\n)=\w[\s\S]*?(\n=cut\s*\n|$)/g,				css: 'comments' },		// pod
 			{ regex: new RegExp(this.getKeywords(funcs), 'gm'),		css: 'functions' },
 			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),	css: 'keyword' }
 		];
