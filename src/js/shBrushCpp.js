93fcf6e0bb0182d501ef1106be8afbc84d2f687b
Stefan Agner <stefan@agner.ch>
2013-02-18 17:34:08 +0100

    Added unsigned to data types

--- src/js/shBrushCpp.js
+++ src/js/shBrushCpp.js
@@ -30,7 +30,7 @@
 						'jmp_buf mbstate_t _off_t _onexit_t _PNH ptrdiff_t _purecall_handler ' +
 						'sig_atomic_t size_t _stat __stat64 _stati64 terminate_function ' +
 						'time_t __time64_t _timeb __timeb64 tm uintptr_t _utimbuf ' +
-						'va_list wchar_t wctrans_t wctype_t wint_t signed';
+						'va_list wchar_t wctrans_t wctype_t wint_t signed unsigned';
 
 		var keywords =	'alignas alignof auto break case catch class const constexpr decltype __finally __exception __try ' +
 						'const_cast continue private public protected __declspec ' +
