4af9fc5951747e88cb47e17a0ff5ff899589dd05
Pushkar Anand <pushkar8723@gmail.com>
2013-12-12 21:53:09 +0530

    Update shBrushPascal.js
    separated data types from keyword and different highlighting for multiline and single line comments

--- src/js/shBrushPascal.js
+++ src/js/shBrushPascal.js
@@ -1,16 +1,23 @@
 ;(function()
 {
-  // CommonJS
+	// CommonJS
 	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
 	function Brush()
-	{
-		var keywords =	"and array begin case const div do downto else end file for forward integer " +
-                       "boolean char function goto if in label mod nil not of or packed procedure " +
-                       "program record repeat set string then to type until var while with";
+	{       
+                var datatypes =	'boolean char integer string';
+		var keywords =	"and array begin case const div do downto else end file for forward " +
+                       "function goto if in label mod nil not of or packed procedure " +
+                       "program record repeat set then to type until var while with";
 		this.regexList = [
-				{ regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' }
-				];
+			{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' },		// one line comments
+			{ regex: /\(\*[\s\S]*?\*\)/gm,                                  css: 'comments' },	 	// multiline comments
+			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },		// strings
+			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// strings
+			{ regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gi,			css: 'value' },			// numbers
+			{ regex: new RegExp(this.getKeywords(datatypes), 'gm'),		css: 'color1 bold' },
+                        { regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }		
+			];
 			
 		this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
 	};
