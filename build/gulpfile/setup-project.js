03da831410cc7495e6dec9677cec919dedb01f96
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-11-25 11:58:52 -0800

    Adds project setup

---
+++ build/gulpfile/setup-project.js
@@ -0,0 +1,76 @@
+import request from 'request';
+import fs from 'fs';
+import R from 'ramda';
+import Promise from 'songbird';
+import childProcess from 'child_process';
+
+export default function (gulp, rootPath) {
+  const REPOS_CACHE = `${rootPath}/.projects-cache.json`;
+  const REPOS_DIR = `${rootPath}/repos`;
+
+  function loadReposFromGitHub() {
+    const opts = {
+      url: 'https://api.github.com/orgs/syntaxhighlighter/repos?per_page=300',
+      json: true,
+      headers: { 'User-Agent': 'node.js' },
+    };
+
+    return new Promise((resolve, reject) =>
+      request(opts, (err, response) => {
+        if (err) return reject(err);
+        const json = response.body;
+        fs.writeFile(REPOS_CACHE, JSON.stringify(json, null, 2));
+        resolve(json);
+      })
+    );
+  }
+
+  const log = value => {
+    console.log(value);
+    return value;
+  }
+
+  const exec = (cmd, opts) =>
+    Promise.resolve(cmd)
+      .then(log)
+      .then(() => childProcess.exec.promise(cmd, opts))
+      .catch(err => { throw new Error(err.message + '\n\n' + cmd) });
+
+  const git = (cmd, cwd) => exec(`git ${cmd}`, {cwd});
+  const loadReposFromCache = () => fs.readFile.promise(REPOS_CACHE, 'utf8').then(JSON.parse);
+  const loadRepos = () => loadReposFromCache().error(loadReposFromGitHub).then(R.map(R.pick(['ssh_url', 'name'])));
+  const cloneRepo = repo => git(`clone '${repo.ssh_url}'`, REPOS_DIR);
+  const pathToRepo = repo => `${REPOS_DIR}/${repo.name}`;
+  const linkNodeModulesIntoRepos = repo => exec(`ln -s ${rootPath}/node_modules ${pathToRepo(repo)}/node_modules || true`);
+  const linkReposIntoNodeModules = repo => exec(`ln -s ${pathToRepo(repo)} ${rootPath}/node_modules || true`)
+
+  gulp.task('setup-project:clone-repos', 'Clones all repositories from SyntaxHighlighter GitHub organization', () =>
+    loadRepos()
+      .then(R.filter(repo => !fs.existsSync(pathToRepo(repo))))
+      .then(R.filter(repo => repo.name !== 'syntaxhighlighter'))
+      .then(R.map(R.curry(cloneRepo)))
+      .then(Promise.all)
+  );
+
+  gulp.task('setup-project:link-node_modules-into-repos', 'Links `./node_modules` into every clonned repository', ['setup-project:clone-repos'], () =>
+    loadRepos()
+      .then(R.map(R.curry(linkNodeModulesIntoRepos)))
+      .then(Promise.all)
+  );
+
+  gulp.task('setup-project:link-repos-into-node_modules', 'Links every clonned repository into `./node_modules`', ['setup-project:clone-repos'], () =>
+    loadRepos()
+      .then(R.map(R.curry(linkReposIntoNodeModules)))
+      .then(Promise.all)
+  );
+
+  gulp.task(
+    'setup-project',
+    'Sets up project for development. RUN THIS FIRST!',
+    [
+      'setup-project:clone-repos',
+      'setup-project:link-node_modules-into-repos',
+      'setup-project:link-repos-into-node_modules'
+    ]
+  );
+}
