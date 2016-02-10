e26bdaf0c4353a4a1f4b2bd29f198b3518f163ca
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2016-02-10 13:51:51 -0800

    Implements global options

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -3,16 +3,16 @@ import {expect} from 'chai';
 
 const HTML = require('raw!./build-source/index.html');
 
-describe('build', function() {
+function createScript(src) {
+  const script = document.createElement('script');
+  script.src = src;
+  return script;
+}
+
+function testSuite() {
   let div;
 
-  function createScript(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    return script;
-  }
-
-  before(function (done) {
+  before(done => {
     div = document.createElement('div');
     div.innerHTML = HTML;
 
@@ -31,10 +31,38 @@ describe('build', function() {
     wait();
   });
 
+  after(() => {
+    document.body.removeChild(div);
+  });
+
   describe('using only the bundle', () => {
     it('highlights v3 brush', () => expect(sizzle('.syntaxhighlighter.html_test_brush_v3')[0]).to.be.ok);
     it('highlights v4 brush', () => expect(sizzle('.syntaxhighlighter.test_brush_v4')[0]).to.be.ok);
   });
 
   it('does not expose window.SyntaxHighlighter', () => expect(window.SyntaxHighlighter).to.be.undefined);
+}
+
+describe('integration', () => {
+  describe('default settings', () => {
+    testSuite();
+  });
+
+  describe('user settings', () => {
+    before(() => {
+      window.syntaxhighlighterConfig = {
+        'class-name': 'foo-bar'
+      };
+    });
+
+    after(() => {
+      delete window.syntaxhighlighterConfig;
+    });
+
+    testSuite();
+
+    it('applies custom class name from global config variable to all units', () =>
+       expect(sizzle('.foo-bar.syntaxhighlighter').length).to.eql(2)
+    );
+  });
 });
